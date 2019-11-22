1565c168415742b674f81a8a7a42a0e7416ab9c2
satyajitdash61 <32846765+satyajitdash61@users.noreply.github.com>
2019-11-22 17:37:13 +0530

    updating

--- client/src/components/welcome/welcome.jsx
+++ client/src/components/welcome/welcome.jsx
@@ -1,9 +1,32 @@
-import React from 'react';
-import './welcome.css';
-function Welcome(){
-    return(
-        <div className="container-fluid WelcomeScreen">Hello World!</div>
-    )
+import React, { useState } from "react";
+import "./welcome.css";
+import logo from "../../images/logo.png";
+function Welcome() {
+  const [login, setLogin] = useState("home");
+
+  return (
+    <div className="container-fluid WelcomeScreen">
+      {
+          login==="home"?
+        <div id="innerdiv">
+          <h1>Go Eats</h1>
+          <img src={logo} />
+          <h4>Log In as </h4>
+          <button id="userbtn" onClick={() => setLogin("res")}>
+            <h5>User</h5>
+          </button>
+          <button id="restbtn" onClick={() => setLogin("user")}>
+            <h5>Restaurant</h5>
+          </button>
+        </div>
+        :
+        login === "res"?
+        <h1>Hello World</h1>
+        :
+        <h1>Satyajit</h1>
+      }
+    </div>
+  );
 }
 
-export default Welcome
+export default Welcome;
