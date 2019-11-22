5c80dd47218ac6a8b98c436c0b86ecc6aa0c24dd
satyajitdash61 <32846765+satyajitdash61@users.noreply.github.com>
2019-11-22 13:09:44 +0530

    component created

---
+++ client/src/components/welcome/welcome.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './welcome.css';
+function Welcome(){
+    return(
+        <div id = "allign">
+            <h1>Welcome</h1>
+        </div>
+    )
+}
+
+export default Welcome
