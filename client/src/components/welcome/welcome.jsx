9e42b58ce36f436990488d7db5a40caf0bbe2fcf
satyajitdash61 <32846765+satyajitdash61@users.noreply.github.com>
2019-11-22 18:29:08 +0530

    updating

--- client/src/components/welcome/welcome.jsx
+++ client/src/components/welcome/welcome.jsx
@@ -1,32 +1,32 @@
-import React, { useState } from "react";
+import React, { Component } from "react";
 import "./welcome.css";
 import logo from "../../images/logo.png";
-function Welcome() {
-  const [login, setLogin] = useState("home");
 
-  return (
-    <div className="container-fluid WelcomeScreen">
-      {
-          login==="home"?
-        <div id="innerdiv">
-          <h1>Go Eats</h1>
-          <img src={logo} />
-          <h4>Log In as </h4>
-          <button id="userbtn" onClick={() => setLogin("res")}>
-            <h5>User</h5>
-          </button>
-          <button id="restbtn" onClick={() => setLogin("user")}>
-            <h5>Restaurant</h5>
-          </button>
-        </div>
-        :
-        login === "res"?
-        <h1>Hello World</h1>
-        :
-        <h1>Satyajit</h1>
-      }
-    </div>
-  );
+class WelcomePage extends Component {
+  
+    constructor(){
+        super();
+        this.state = {
+            userLogIn:"res"
+        }
+    }
+
+  render() {
+    return (
+      <div className="container-fluid WelcomeScreen">
+          <div id="innerdiv">
+            <h1>Go Eats</h1>
+            <img src={logo} />
+            <h4>Log In as </h4>
+            <button id="userbtn" >
+              <h5>User</h5>
+            </button>
+            <button id="restbtn" >
+              <h5>Restaurant</h5>
+            </button>
+          </div>
+      </div>
+    );
+  }
 }
-
-export default Welcome;
+export default WelcomePage;
