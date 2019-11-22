6d3c2cc8c763ad5814fe1d6543ad095eec2c54e7
satyajitdash61 <32846765+satyajitdash61@users.noreply.github.com>
2019-11-22 18:55:22 +0530

    updating

---
+++ client/src/components/UserLogIn/UserLogin.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class UserLoginPage extends Component {
+    render(){
+        return  (
+            <div>
+                <p>Hello</p>
+            </div>
+        )
+    }
+}
+export default UserLoginPage;
--- client/src/components/welcome/welcome.jsx
+++ client/src/components/welcome/welcome.jsx
@@ -1,30 +1,39 @@
 import React, { Component } from "react";
 import "./welcome.css";
 import logo from "../../images/logo.png";
+import UserLoginPage from'../UserLogIn/UserLogin'
 
 class WelcomePage extends Component {
   
     constructor(){
         super();
         this.state = {
-            userLogIn:"res"
+            userLogIn:"homepage"
         }
     }
 
   render() {
     return (
+        
       <div className="container-fluid WelcomeScreen">
+          {
+        this.state.userLogIn === "homepage"?
           <div id="innerdiv">
             <h1>Go Eats</h1>
             <img src={logo} />
             <h4>Log In as </h4>
-            <button id="userbtn" >
+            <button id="userbtn" onClick={() => this.setState({userLogIn:"User"})}>
               <h5>User</h5>
             </button>
-            <button id="restbtn" >
+            <button id="restbtn" onClick={() => this.setState({userLogIn:"Restaurant"})}>
               <h5>Restaurant</h5>
             </button>
-          </div>
+          </div> 
+          : this.state.userLogIn === "User"?
+           <UserLoginPage/>
+          :
+          <h1>Restaurant</h1>
+         }
       </div>
     );
   }
