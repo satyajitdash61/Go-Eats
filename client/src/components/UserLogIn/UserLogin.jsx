0f05da4f29c7122209c5a690657194ba6a229044
satyajitdash61 <32846765+satyajitdash61@users.noreply.github.com>
2019-11-23 15:55:52 +0530

    userlogin page

--- client/src/components/UserLogIn/UserLogin.jsx
+++ client/src/components/UserLogIn/UserLogin.jsx
@@ -1,10 +1,10 @@
-import React, { Component } from 'react';
+ import React, { Component } from 'react';
 
 class UserLoginPage extends Component {
     render(){
         return  (
-            <div>
-                <p>Hello</p>
+            <div className="conatiner-fluid main1">
+                
             </div>
         )
     }
