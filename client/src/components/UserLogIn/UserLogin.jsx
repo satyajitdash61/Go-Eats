9164b45d32a9b8ba48e603fccbfa1e8bdf835c04
satyajitdash61 <32846765+satyajitdash61@users.noreply.github.com>
2019-11-24 01:27:49 +0530

    adding design

--- client/src/components/UserLogIn/UserLogin.jsx
+++ client/src/components/UserLogIn/UserLogin.jsx
@@ -5,6 +5,7 @@ import navlogo from "../../images/navlogo.png";
 class UserLoginPage extends Component {
   render() {
     return (
+        <div>
       <div className="container-fluid main">
         <nav>
           <img src={navlogo} alt="" id="navlogo"/>
@@ -13,8 +14,23 @@ class UserLoginPage extends Component {
           </ul>
         </nav>
         <div id="tagline">
+            <span id="go">Go </span><span id="eats">Eats</span><br/><br/>
+            <span id="hunger">Hunger ?</span><br/>
+            <span id="tag">Want to enjoy your Food?</span> <br/>
+            <span id="tag1">Just GoEats it !</span>
+        </div>
+      </div>
+      <div className="container-fluid characteristics">
+        <div id="quality_foods">
+            
+        </div>
+        <div id="fast_delivery">
 
         </div>
+        <div id="original_recipes">
+            
+        </div>
+      </div>
       </div>
     );
   }
