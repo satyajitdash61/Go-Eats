dc40faf3fd40d81d04f8371ebbfa23c9759981be
satyajitdash61 <32846765+satyajitdash61@users.noreply.github.com>
2019-11-23 21:11:00 +0530

    alignment

--- client/src/components/UserLogIn/UserLogin.jsx
+++ client/src/components/UserLogIn/UserLogin.jsx
@@ -1,19 +1,22 @@
- import React, { Component } from 'react';
- import './Userlogin.css';
+import React, { Component } from "react";
+import "./Userlogin.css";
+import navlogo from "../../images/navlogo.png";
 
 class UserLoginPage extends Component {
-    render(){
-        return  (
-            <div className="container-fluid main">
-                <nav>
-                    <ul>
-                        <li>
-                            Sign in
-                        </li>
-                    </ul>
-                </nav>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="container-fluid main">
+        <nav>
+          <img src={navlogo} alt="" id="navlogo"/>
+          <ul>
+            <li>Sign in</li>
+          </ul>
+        </nav>
+        <div id="tagline">
+
+        </div>
+      </div>
+    );
+  }
 }
 export default UserLoginPage;
