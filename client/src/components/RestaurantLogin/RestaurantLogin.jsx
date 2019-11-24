1b222c6fd6b3088afb12eb806fe975bddebab5e9
satyajitdash61 <32846765+satyajitdash61@users.noreply.github.com>
2019-11-24 16:51:46 +0530

    restaurant log in page in progress

--- client/src/components/RestaurantLogin/RestaurantLogin.jsx
+++ client/src/components/RestaurantLogin/RestaurantLogin.jsx
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
+import './RestaurantLogin.css';
+import restaurantpic from '../../images/cherry-delivery.png'
 class RestaurantLoginPage extends Component {
     render() { 
         return ( 
-            <div>
-                <p>Hello , This is restaurant</p>
+            <div className="mainscreen">
+                <div id="leftside">
+                <img src={restaurantpic} alt="" id="restaurantpic"/>
+                </div>
+                <div id="rightside">
+                    
+                </div>
             </div>
          );
     }
