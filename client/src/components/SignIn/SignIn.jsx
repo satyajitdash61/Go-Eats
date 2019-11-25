b73528951f3d5852fd95f72149c81e1a32bc28ce
satyajitdash61 <32846765+satyajitdash61@users.noreply.github.com>
2019-11-25 18:09:22 +0530

    updating

--- client/src/components/SignIn/SignIn.jsx
+++ client/src/components/SignIn/SignIn.jsx
@@ -5,11 +5,26 @@ import SignUp from '../SignUp/SignUp';
 constructor(){
     super();
     this.state = {
+        id:"",
+        password:"",
         page1: "thispage"
     };
 }
+    handelChange = event => {
+        const{name,value} = event.target
+        this.setState({
+            [name] : value
+        })
+    }
+    handleSubmit = () =>{
+        
+        console.log(this.state.id)
+        console.log(this.state.password)
 
+        
+    }
      render() { 
+         const {id,password} = this.state;
          return ( 
              <div>
                  {this.state.page1 === "thispage" ? (
@@ -20,9 +35,9 @@ constructor(){
             <div id="inputarea1">
                  <h2>Welcome User</h2><br/>
                  <p id="info1">Sign in with your id and password.</p>
-                 <input type="text" name="id1" id="id1" placeholder="User Id"/>
-                 <input type="password" name="password1" id="password1" placeholder="Password"/>
-                 <button id="btn3">Sign in</button>
+                 <input type="text" name="id" value={id} onChange={this.handelChange} id="id1" placeholder="User Id"/>
+                 <input type="password" name="password" value={password} onChange={this.handelChange} id="password1" placeholder="Password"/>
+                 <button id="btn3" onClick={this.handleSubmit}>Sign in</button>
                  <div id="box1">
                  <p id="no_acc">Do not have an account? <span id="signuplink" onClick={()=>this.setState({page1:"thatpage"})}>Sign Up</span></p>
                  
