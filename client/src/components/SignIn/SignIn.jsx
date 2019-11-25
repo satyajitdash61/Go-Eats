4ae3aac0020d6accf92a9ceee76a3d5e36f47232
satyajitdash61 <32846765+satyajitdash61@users.noreply.github.com>
2019-11-25 12:32:16 +0530

    updating

--- client/src/components/SignIn/SignIn.jsx
+++ client/src/components/SignIn/SignIn.jsx
@@ -2,11 +2,11 @@ import React, { Component } from 'react';
 import './SignIn.css';
  class SignIn extends Component {
 
-    
+
      render() { 
          return ( 
              
-             <h1>HEllo</h1>
+             <p>bjn</p>
             
           );
      }
--- client/src/components/UserLogIn/UserLogin.jsx
+++ client/src/components/UserLogIn/UserLogin.jsx
@@ -1,65 +1,72 @@
 import React, { Component } from "react";
 import "./Userlogin.css";
-import SignIn from '../SignIn/SignIn'
+import SignIn from "../SignIn/SignIn";
 import navlogo from "../../images/navlogo.png";
 import goodfood from "../../images/goodfood.png";
 import recipe from "../../images/recipe.png";
-import takeaway from "../../images/take_away.png"
+import takeaway from "../../images/take_away.png";
 
 class UserLoginPage extends Component {
-constructor(){
-  super();
-  this.state = {
-    page : "thispage"
+  constructor() {
+    super();
+    this.state = {
+      page: "thispage"
+    };
   }
-}
 
   render() {
     return (
-        <div>
-{
-  this.state.page === "thispage" ?
-  <div>
-      <div className="container-fluid main">
-        <nav>
-          <img src={navlogo} alt="" id="navlogo"/>
-          <ul>
-            <li id="sign-in-button" onClick={()=>this.setState({page:"thatpage"})}>Sign in</li>
-          </ul>
-        </nav>
-        <div id="tagline">
-            <span id="go">Go </span><span id="eats">Eats</span><br/><br/>
-            <span id="hunger">Hunger ?</span><br/>
-            <span id="tag">Want to enjoy your Food?</span> <br/>
-            <span id="tag1">Just GoEats it !</span>
-        </div>
+      <div>
+        {this.state.page === "thispage" ? (
+          <div>
+            <div className="container-fluid main">
+              <nav>
+                <img src={navlogo} alt="" id="navlogo" />
+                <ul>
+                  <li
+                    id="sign-in-button"
+                    onClick={() => this.setState({ page: "thatpage" })}
+                  >
+                    Sign in
+                  </li>
+                </ul>
+              </nav>
+              <div id="tagline">
+                <span id="go">Go </span>
+                <span id="eats">Eats</span>
+                <br />
+                <br />
+                <span id="hunger">Hunger ?</span>
+                <br />
+                <span id="tag">Want to enjoy your Food?</span> <br />
+                <span id="tag1">Just GoEats it !</span>
+              </div>
+            </div>
+            <div id="service">
+              <span id="service_style">Our Services</span>
+            </div>
+            <div className="characteristics">
+              <div id="quality_foods">
+                <img src={goodfood} alt="" id="goodfood" />
+                <h5>Quality Foods</h5>
+              </div>
+              <div id="original_recipes">
+                <img src={recipe} alt="" id="recipe" />
+                <h5>Good Recipe</h5>
+              </div>
+              <div id="delivery">
+                <img src={takeaway} alt="" id="takeaway" />
+                <h5>Take Away</h5>
+              </div>
+            </div>
+            <div id="footer">
+              <p>&#169; 2019 Go Eats Inc.</p>
+            </div>
+          </div>
+        ) : (
+          <SignIn />
+        )}
       </div>
-      <div id="service">
-          <span id="service_style">Our Services</span>
-      </div>
-      <div className="characteristics">
-        <div id="quality_foods">
-            <img src={goodfood} alt="" id="goodfood"/>
-            <h5>Quality Foods</h5>
-        </div>
-        <div id="original_recipes">
-        <img src={recipe} alt="" id="recipe"/>
-            <h5>Good Recipe</h5>
-        </div>
-        <div id="delivery">
-        <img src={takeaway} alt="" id="takeaway"/>
-            <h5>Take Away</h5>
-        </div>
-      </div>
-      <div id="footer">
-          <p>&#169; 2019 Go Eats Inc.</p>
-      </div>
-      </div>
-        : <SignIn/>
-
-      }
-      </div>
-      
     );
   }
 }
