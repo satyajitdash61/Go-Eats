62b604f10ddb667d1bd76b53710dda081a176211
satyajitdash61 <32846765+satyajitdash61@users.noreply.github.com>
2019-11-27 01:08:34 +0530

    updating

---
+++ server/Models/usermodel.js
@@ -0,0 +1,17 @@
+const mongoose=require('mongoose');
+
+const userSchema= new mongoose.Schema({
+
+
+    id:{
+       type: String,
+       required:true
+    },
+    password:{
+        type:String,
+        required:true,
+        // minlength:8
+    }
+});
+
+module.exports=mongoose.model('User',userSchema);
