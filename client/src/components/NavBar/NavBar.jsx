b4ea3be4ac47167c200f1e4cf2c85ba32e585724
satyajitdash61 <32846765+satyajitdash61@users.noreply.github.com>
2019-11-28 11:41:55 +0530

    adding nav bar component

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,2 +1,20 @@
-import React, { Component } from 'react';
-import './NavBar.css';
+import React, { Component } from "react";
+import "./NavBar.css";
+import navlogo from "../../images/navlogo.png";
+
+class NavBar extends Component {
+  render() {
+    return (
+      <div className="">
+        <nav>
+          <img src={navlogo} alt="" id="navlogo" />
+          <ul>
+            <li id="sign-in-button"> Sign in  </li>           
+          </ul>
+        </nav>
+      </div>
+    );
+  }
+}
+
+export default NavBar;
